643bc9792a30627f53953966aba054dcab465fa1
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-04-15 12:58:38 +0200

    fix: change createAt to creactedAt

---
+++ pages/api/route/index.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { getAllUsers, getUser } from "@/prisma";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    switch (req.method) {
+      case "GET": {
+        if (req.query.id) {
+          const id = req.query.id as string;
+          console.log("req.query.id", req.query.id);
+
+          const user = await getUser(id);
+
+          return res.send(user);
+        } else {
+          const users = await getAllUsers();
+
+          return res.send(users);
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e, "err");
+  }
+};
+
+export default handler;
