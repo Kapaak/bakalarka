600457c79c85078ec476a64603e89b89fe4c1c65
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-08-02 13:05:54 +0200

    feat: optimised build

--- next.config.js
+++ next.config.js
@@ -6,4 +6,8 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withBundleAnalyser = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyser(nextConfig);
